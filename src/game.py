1bbd9a1b5ba8f6c12f2b4be6692041fa82408487
Jan Rubel <jasiekrubel@gmail.com>
2020-10-22 15:44:50 +0200

    Add base class for pieces Update gitignore for VSC

--- src/game.py
+++ src/game.py
@@ -1,15 +1,27 @@
 import pygame
+from pieces import Bishop, Rook
+
 
 def main():
     pygame.init()
-    pygame.display.set_mode((400,400))
-
+    screen = pygame.display.set_mode((800, 800))
+    clock = pygame.time.Clock()
     running = True
+    sprites_group = pygame.sprite.Group()
+    rook1 = Rook('B', 0, 0)
+    rook2 = Rook('W', 1, 0)
+    bishop1 = Bishop('W', 2, 0)
+
+    sprites_group.add([rook1, rook2, bishop1])
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        pygame.display.flip()
+        sprites_group.draw(screen)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
---
+++ src/pieces.py
@@ -0,0 +1,47 @@
+import pygame
+
+
+class Piece(pygame.sprite.Sprite):
+    symbol = None
+    piece_name = None
+
+    def __init__(self, color, x, y):
+        super().__init__()
+        self.color = color
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(
+            "assets/pieces/{}{}.png".format(self.piece_name, self.color))
+        self.rect = self.image.get_rect()
+        self.rect.x = 20 + 80*x
+        self.rect.y = 20 + 80*y
+
+
+class Bishop(Piece):
+    symbol = 'B'
+    piece_name = "Bishop"
+
+
+class King(Piece):
+    symbol = 'K'
+    piece_name = 'King'
+
+
+class Knight(Piece):
+    symbol = 'N'
+    piece_name = 'Knight'
+
+
+class Pawn(Piece):
+    symbol = 'P'
+    piece_name = 'Pawn'
+
+
+class Queen(Piece):
+    symbol = 'Q'
+    piece_name = 'Queen'
+
+
+class Rook(Piece):
+    symbol = 'R'
+    piece_name = 'Rook'
