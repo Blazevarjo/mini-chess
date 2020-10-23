ad13acbb9e4453c11480569475b429dea65b01f7
Jan Rubel <jasiekrubel@gmail.com>
2020-10-23 22:35:57 +0200

    Fix vars for screen dimensions and board offsets

--- src/game.py
+++ src/game.py
@@ -1,14 +1,19 @@
 import pygame
 from board import Board
-from pieces import Bishop, Rook
+
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 800
 
 
 def main():
     pygame.init()
 
-    screen = pygame.display.set_mode((900, 800))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     board = Board()
+
+    board_offset_x = (screen.get_width() - board.get_width()) // 2
+    board_offset_y = (screen.get_height() - board.get_height()) // 2
 
     running = True
     while running:
@@ -19,7 +24,7 @@ def main():
         # sprites_group.draw(screen)
         pygame.display.flip()
 
-        screen.blit(board, (200, 100))
+        screen.blit(board, (board_offset_x, board_offset_y))
 
         clock.tick(60)
 
